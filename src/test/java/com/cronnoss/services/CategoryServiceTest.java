bb8ce81f3e7703345e569af012332b4252805ecc
Evgeniy Karasev <karasevevg@gmail.com>
2021-12-13 22:56:32 +0300

    7. Category Service

---
+++ src/test/java/com/cronnoss/services/CategoryServiceTest.java
@@ -0,0 +1,66 @@
+package com.cronnoss.services;
+
+import com.cronnoss.api.v1.mapper.CategoryMapper;
+import com.cronnoss.api.v1.model.CategoryDTO;
+import com.cronnoss.domain.Category;
+import com.cronnoss.repositories.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+class CategoryServiceTest {
+
+    public static final Long ID = 2L;
+    public static final String NAME = "Jimmy";
+    CategoryService categoryService;
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        categoryService = new CategoryServiceImpl(CategoryMapper.INSTANCE, categoryRepository);
+    }
+
+    @Test
+    public void getAllCategories() {
+
+        //given
+        List<Category> categories = Arrays.asList(new Category(), new Category(), new Category());
+
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        //when
+        List<CategoryDTO> categoryDTOS = categoryService.getAllCategories();
+
+        //then
+        Assertions.assertEquals(3, categoryDTOS.size());
+    }
+
+    @Test
+    public void getCategoryByName() {
+        //given
+        Category category = new Category();
+        category.setId(ID);
+        category.setName(NAME);
+
+        when(categoryRepository.findByName(anyString())).thenReturn(category);
+
+        //when
+        CategoryDTO categoryDTO = categoryService.getCategoryByName(NAME);
+
+        //then
+        Assertions.assertEquals(ID, categoryDTO.getId());
+        Assertions.assertEquals(NAME, categoryDTO.getName());
+    }
+}
